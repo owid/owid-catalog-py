808096029deffeaba3e688d25eb363b40a3e8fb2
Marigold <mojmir.vinkler@gmail.com>
2022-04-05 11:16:11 +0200

    fix(utils): fix underscore functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
-from owid.catalog.utils import underscore
+import pandas as pd
+from owid.catalog import Table
+from owid.catalog.utils import underscore, underscore_table
 
 
 def test_underscore():
@@ -30,3 +32,42 @@ def test_underscore():
         underscore("Automated teller machines (ATMs) (per 100,000 adults)")
         == "automated_teller_machines__atms__per_100_000_adults"
     )
+    assert (
+        underscore(
+            "Political regimes - OWID based on Boix et al. (2013), V-Dem (v12), and Lührmann et al. (2018)"
+        )
+        == "political_regimes__owid_based_on_boix_et_al__2013__v_dem__v12__and_luhrmann_et_al__2018"
+    )
+    assert (
+        underscore("Adjusted savings: particulate emission damage (current US$)")
+        == "adjusted_savings__particulate_emission_damage__current_usd"
+    )
+    assert (
+        underscore(
+            "Benefit incidence of unemployment benefits and ALMP to poorest quintile (% of total U/ALMP benefits)"
+        )
+        == "benefit_incidence_of_unemployment_benefits_and_almp_to_poorest_quintile__pct_of_total_u_almp_benefits"
+    )
+    assert (
+        underscore(
+            "Business extent of disclosure index (0=less disclosure to 10=more disclosure)"
+        )
+        == "business_extent_of_disclosure_index__0_less_disclosure_to_10_more_disclosure"
+    )
+    assert (
+        underscore("Firms that spend on R&D (% of firms)")
+        == "firms_that_spend_on_r_and_d__pct_of_firms"
+    )
+
+
+def test_underscore_table():
+    df = pd.DataFrame({"A": [1, 2, 3]})
+    df.index.names = ["I"]
+
+    t = Table(df)
+    t["A"].metadata.description = "column A"
+
+    tt = underscore_table(t)
+    assert tt.columns == ["a"]
+    assert tt.index.names == ["i"]
+    assert tt["a"].metadata.description == "column A"
